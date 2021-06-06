b33dbc71fe360713b776b3e97b8b5ad9c3c1b193
mwaitzman <51432220+mwaitzman@users.noreply.github.com>
2021-06-06 02:28:57 -0400

    Code actually compiles now
    Code compiles now and should be more robust and idiomatic.

--- src/main.rs
+++ src/main.rs
@@ -2,59 +2,63 @@ use std::io::stdin;
 use ureq::*;
 
 fn main() {
+    let mut input_string = String::new();
     let mut client = Client {
         agent: ureq::AgentBuilder::new().build(),
         base_url: String::from("https://api.dnsdb.info/"),
-        input_string: String::new(),
         input_history: Vec::new(),
         API_KEY: None,
     };
     let ir = stdin();
     loop {
-        ir.read_line(&mut client.input_string);
-        client.input_history.push(client.input_string.clone());
-        client.handle_input(client.input_string.split_whitespace().collect::<_>());
+        ir.read_line(&mut input_string);
+        client.input_history.push(input_string.clone());
+        client.handle_input(input_string.split_whitespace());
+        input_string.clear();
     }
 }
 
 pub(crate) struct Client {
     agent: ureq::Agent,
     base_url: String,
-    input_string: String,
     input_history: Vec<String>,
     API_KEY: Option<String>,
 }
 impl Client {
-    pub fn handle_input(&mut self, tokens: Vec<String>) {
-        match &tokens[0] {
+    pub fn handle_input<'a>(&mut self, mut tokens: impl Iterator<Item = &'a str>) {
+        let token = tokens.next().unwrap();
+        match token {
             "ping" => {
                 self.ping();
                 return;
             }
             "set" => {
-                self.set(&tokens[1..]);
+                self.set(tokens);
                 return;
             }
             _ => {
-                println!("Unrecognized token: \"{token}\".", token = tokens[0]);
+                println!("Unrecognized token: \"{token}\".", token = token);
                 return;
             }
         }
     }
 
     fn ping(&mut self) {
-        let response = self.agent.get(format!("{}dnsdb/v2/ping", self.base_url));
-        println!("Response: {response}.", response = response.to_string());
+        let response = self
+            .agent
+            .get(&(self.base_url.clone() + &String::from("dnsdb/v2/ping")));
+        println!("Response: {response:?}.", response = response);
     }
 
-    fn set(&self, input: &[String]) {
-        match input[0] {
+    fn set<'a>(&mut self, mut input: impl Iterator<Item = &'a str>) {
+        let token = input.next().unwrap();
+        match token {
             "API_KEY" => {
-                self.API_KEY = Some(input[1].clone());
+                self.API_KEY = Some(input.next().unwrap().to_owned());
                 return;
             }
             _ => {
-                println!("Error: token \"{token}\" is invalid.", token = input[0]);
+                println!("Error: token \"{token}\" is invalid.", token = token);
             }
         }
     }
